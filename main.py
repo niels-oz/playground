2c6ffc343dab45a5d63959c6aeb17fbf7dd4ac6a
niels-oz <enbeoh@gmail.com>
2023-01-26 17:47:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import pandas as pd
 
-df = pd.DataFrame(data=[[1,.51,0,.8,1,.29],[0,.51,0,.8,1,.29],[0,.51,1,.8,0,.29]],
-                  columns=['r1','r1_weight','r2','r2_weight','r3','r3_weight'])
+df = pd.DataFrame(data=[[1,0,1],[0,0,1],[0,1,0]],
+                  columns=['r1','r2','r3'])
 
 list_rules = ['r1', 'r2', 'r3']
-list_weights = ['r1_weight', 'r2_weight', 'r3_weight']
 
-dict = zip(list_rules,list_weights)
+dict_weights = {
+    'r1_weight': 0.51,
+    'r2_weight': 0.8,
+    'r3_weight': 0.29
+}
 
-for rule, weight in dict:
-    df[rule+'_result'] = df[rule] * df[weight]
+df['result'] = 0
+
+for rule in list_rules:
+    df['result'] += df[rule].multiply(dict_weights.get(rule+'_weight'))
 
 print(df)
